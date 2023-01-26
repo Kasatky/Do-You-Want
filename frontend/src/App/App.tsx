b245e63212b55f27ac7428227c1fda9b89afe27e
Irina Kasatkina <opacopa@inbox.ru>
2023-01-26 19:48:34 +0300

    Add folders in frontend

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import Login from './Login';
-import Registration from './Registration';
+import Login from '../Auth/Login';
+import Registration from '../Auth/Registration';
 
 
 function App() {
