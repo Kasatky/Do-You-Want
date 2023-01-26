21cfff961dffd2f17b399a684996191967316025
Irina Kasatkina <opacopa@inbox.ru>
2023-01-26 16:05:09 +0300

    Prepare for deploy

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,16 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 
-const { PORT } = process.env;
+const { PORT } = process.env || 3000;
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, '../../frontend/build')));
+app.get('/', (req, res) => res.json({ message: 'ok' }));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+});
 
 app.listen(PORT, () => console.log('Server listen ', PORT));
