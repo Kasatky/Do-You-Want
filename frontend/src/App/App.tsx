350523168e66c4a99f13b92e98b087e0e4b93df7
Anastasiya <iinokayo@mail.ru>
2023-01-26 20:57:28 +0300

    Auth modal done

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -1,16 +1,8 @@
 import React from 'react';
 import './App.css';
-import Login from '../Auth/Login';
-import Registration from '../Auth/Registration';
-
 
 function App() {
-  return (
-    <div className="App">
-      <Registration />
-      <Login />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
