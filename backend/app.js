f5ccc28fd0133157eaf05ee1ba97a09ba438b0be
Irina Kasatkina <opacopa@inbox.ru>
2023-01-26 20:01:58 +0300

    fix app route config for "/" url

--- backend/app.js
+++ backend/app.js
@@ -15,14 +15,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(session(sessionConfig));
 
-
-app.use(express.static(path.join(__dirname, '../../frontend/build')));
-app.get('/', (req, res) => res.json({ message: 'ok' }));
 app.use('/api/auth', authRouter);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
 });
+
+app.use(express.static(path.join(__dirname, '../../frontend/build')));
 
 app
   .listen(PORT)
