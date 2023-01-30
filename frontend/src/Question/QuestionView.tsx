c66c337b006d7a0362a08d0b37462b9d2cf7b9ab
Irina Kasatkina <opacopa@inbox.ru>
2023-01-30 22:11:45 +0300

    Create question on page and fix error

--- frontend/src/Question/QuestionView.tsx
+++ frontend/src/Question/QuestionView.tsx
@@ -1,57 +1,42 @@
-import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
-import Zoom from '@mui/material/Zoom';
 import { Typography } from '@mui/material';
 import { useSelector } from 'react-redux';
 import { RootState, useAppDispatch } from '../store';
 import { getRandomWish } from '../wishSlice';
-import { Wish } from '../wishTypes';
 
 export default function QuestionView() {
-  // const [checked, setChecked] = useState(true);
-  const [wishObj] = useSelector((state: RootState) => state.wish.list);
+  const arr = useSelector((state: RootState) => state.wish.list);
 
   let wish;
-  if (wishObj) {
-    wish = wishObj.wish;
+  if (arr && arr.length) {
+    wish = arr[0].wish;
   }
-
-  console.log(wish);
 
   const dispatch = useAppDispatch();
 
   const handleFalse = () => {
-    // setChecked(false);
     dispatch(getRandomWish());
   };
   const handleTrue = () => {
-    // setChecked(false);
     dispatch(getRandomWish());
   };
-  // const icon = (
-  //   <Paper sx={{ m: 1 }} elevation={4}>
-  //     <Typography>
-  //       {wish
-  //         ? wish
-  //         : 'Вау! Вы перебрали все вопросы, совсем скоро появятся новые или можете добавить свои, нажав на кнопку "Добавить вопрос"'}
-  //     </Typography>
-  //     <Box component="svg" sx={{ width: 500, height: 200 }}></Box>
-  //   </Paper>
-  // );
 
   return (
-    <Box sx={{ height: 180 }}>
-      <div>{wish}</div>
+    <Box sx={{ height: '180px' }}>
+      <Paper sx={{ m: 1 }} elevation={4}>
+        <Box component="h1" sx={{ width: 500, height: 50 }}>
+          {wish
+            ? `Хочешь ${wish}`
+            : 'Вау! Вы перебрали все вопросы, совсем скоро появятся новые или можете добавить свои, нажав на кнопку "Добавить вопрос"'}
+        </Box>
+      </Paper>
       <button type="button" onClick={handleTrue}>
-        Yes
+        Да
       </button>
       <button type="button" onClick={handleFalse}>
-        No
+        Нет
       </button>
-      {/* <Box sx={{ display: 'flex' }}>
-        <Zoom in={checked}>{icon}</Zoom>
-      </Box> */}
     </Box>
   );
 }
