168fc48b28def8187e6ddb9bcb69ded9082a6069
Anastasiya <iinokayo@mail.ru>
2023-01-31 11:20:16 +0300

    Fixed QuestionView, wishSlice and wishRouter

--- frontend/src/Question/QuestionView.tsx
+++ frontend/src/Question/QuestionView.tsx
@@ -1,42 +1,45 @@
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import { Typography } from '@mui/material';
+import React, { useEffect } from 'react';
+import { Box, Paper, Button } from '@mui/material';
 import { useSelector } from 'react-redux';
 import { RootState, useAppDispatch } from '../store';
 import { getRandomWish } from '../wishSlice';
 
 export default function QuestionView() {
-  const arr = useSelector((state: RootState) => state.wish.list);
-
-  let wish;
-  if (arr && arr.length) {
-    wish = arr[0].wish;
-  }
-
   const dispatch = useAppDispatch();
+  const random = useSelector((state: RootState) => state.wish.random);
+
+  useEffect(() => {
+    if (!random) {
+      dispatch(getRandomWish());
+    }
+  }, [dispatch, random]);
 
   const handleFalse = () => {
     dispatch(getRandomWish());
   };
+
   const handleTrue = () => {
     dispatch(getRandomWish());
   };
 
   return (
     <Box sx={{ height: '180px' }}>
-      <Paper sx={{ m: 1 }} elevation={4}>
-        <Box component="h1" sx={{ width: 500, height: 50 }}>
-          {wish
-            ? `Хочешь ${wish}`
-            : 'Вау! Вы перебрали все вопросы, совсем скоро появятся новые или можете добавить свои, нажав на кнопку "Добавить вопрос"'}
+      <Paper>
+        <Box component="h1">
+          {
+            random && `Хочешь ${random.wish}`
+            // то, что ниже можно добавить, когда будет реализовано, чтобы вопросы не повторялись
+            // а если надо, чтобы они повторялись, то и строка ниже не нужна
+            // : 'Вау! Вы перебрали все вопросы, совсем скоро появятся новые или можете добавить свои, нажав на кнопку "Добавить вопрос"'
+          }
         </Box>
       </Paper>
-      <button type="button" onClick={handleTrue}>
+      <Button variant="contained" onClick={handleTrue}>
         Да
-      </button>
-      <button type="button" onClick={handleFalse}>
+      </Button>
+      <Button variant="contained" onClick={handleFalse}>
         Нет
-      </button>
+      </Button>
     </Box>
   );
 }
