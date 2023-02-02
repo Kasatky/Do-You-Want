deee3aea14c4ec5ca849666c138bfc5012e63ec4
Anastasiya <iinokayo@mail.ru>
2023-02-02 15:39:27 +0300

    Adding new notes in diary

--- backend/routes/diaryRouter.js
+++ backend/routes/diaryRouter.js
@@ -14,4 +14,24 @@ diaryRouter.get('/', async (req, res) => {
   }
 });
 
+diaryRouter.post('/new', async (req, res) => {
+  const { userId } = req.session;
+  const { situation, emotion, mind, action } = req.body;
+
+  try {
+    const newNote = await DiaryEmotion.create({
+      userId,
+      situation,
+      emotion,
+      mind,
+      action,
+    });
+
+    res.json(newNote);
+  } catch (error) {
+    console.log(`Ошибка при добавлении записи: ${error.message}`);
+    res.status(500).json({ error: 'Ошибка при добавлении записи' });
+  }
+});
+
 module.exports = diaryRouter;
